fc89aff103c159450005afea15e6c55402beaa22
Fadhola Asandi <mardikha.555@gmail.com>
2024-06-07 14:05:26 +0700

    memperbaiki directory

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('data.json')
+    fetch('./json/data.json')
         .then(response => response.json())
         .then(data => {
             const discountSalesData = data.discountSalesData;
